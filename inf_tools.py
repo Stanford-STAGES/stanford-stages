00255bdbff9701ad81a68e175a3c3521f17c3208
informaton <hyattmoore@gmail.com>
2018-07-18 15:17:05 -0700

    Initial source code drop
    Former-commit-id: 963836bb9b5e40b6a57c9cfb4b6881e87716a218

---
+++ inf_tools.py
@@ -0,0 +1,6 @@
+#from scipy.fftpack import fft
+def myprint(str,*args):
+    silent = True
+    silent = False
+    if not silent:
+        print(str,*args) #  print(*args) - also works if we goto myprint(*args)
