a9da84fc3f4c8f0cf414e0ff9c9eaa4231ced567
informaton <hyattmoore@gmail.com>
2018-07-18 15:17:05 -0700

    Initial source code drop

---
+++ inf_narco_app.py
@@ -0,0 +1,248 @@
+# -*- coding: utf-8 -*-
+"""
+@author: jens
+@modifier: informaton
+
+inf_narco_biomarker --> inf_narco_app
+"""
+import os
+import sys
+from datetime import datetime
+import json     # for command line interface input and output.
+
+import pdb
+import warnings
+warnings.simplefilter('ignore',FutureWarning)  #warnings.filterwarnings("ignore")
+
+# for getting predictions
+import numpy as np
+import scipy.io as sio
+import gpflow as gpf
+import tensorflow as tf
+
+import random
+
+# For hypnodensity plotting ...
+import matplotlib
+# matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.patches import Polygon
+
+from inf_tools import myprint
+from inf_hypnodensity import Hypnodensity  # from inf_extract_features import ExtractFeatures --> moved to inf_hypnodensity.py
+from inf_config import AppConfig # for AppConfig() <-- narco_biomarker(), [previously]
+
+
+DEBUG_MODE = False
+STANDARD_EPOCH_SEC = 30
+DEFAULT_SECONDS_PER_EPOCH = 30
+DEFAULT_MINUTES_PER_EPOCH = 0.5 #  30/60 or DEFAULT_SECONDS_PER_EPOCH/60;
+
+
+def main(edfFilename, configInput): # configInput is object with additional settings.   'channel_indices', 'lightsOff','lightsOn'
+
+    # Application settings
+    appConfig = AppConfig()
+    appConfig.edf_path = edfFilename;
+
+    channel_categories = {
+        'central': 'C3',
+        'occipital': 'O1',
+        'eog_l': 'EOG-L',
+        'eog_r': 'EOG-R',
+        'chin_emg': 'EMG'
+    }
+
+    # configInput.channel_labels['central','occipital','eog_l','eog_r','chin_emg']
+    # configInput.channel_indices
+    for channel_category,channel_index in configInput["channel_indices"].items():
+        channel_label = channel_categories.get(channel_category,None)
+        if channel_label is not None:
+            appConfig.channels_used[channel_label] = channel_index
+
+    appConfig.lightsOff = configInput.get('lightsOff',33)
+    appConfig.lightsOn = configInput.get('lightsOn',1152)
+    #appConfig.showPlot = configInput.get('hypnodensity.showplot',False)
+    hyp = {}
+    hyp['showplot'] = False
+    hyp['saveplot'] = False
+    hyp['showtext'] = False
+    hyp['savetext'] = False
+    hyp['savename'] = edfFilename
+    hyp.update(configInput.get('hypnodensity',{}))
+    hypnoConfig = hyp
+
+    narcoApp = NarcoApp(appConfig)
+
+    # narcoApp.eval_all()
+    narcoApp.eval_hypnodensity()
+
+    print('AppConfig.showPlot is ',hypnoConfig["showplot"])
+    print('AppConfig.saveplot is ',hypnoConfig["saveplot"])
+
+
+    renderHypnodensity(narcoApp.get_hypnodensity(), showPlot = hypnoConfig['showplot'],
+                        savePlot = hypnoConfig['saveplot'], fileName = hypnoConfig['savename'])
+    prediction = narcoApp.get_narco_prediction()
+
+    print(prediction)
+
+def renderHypnodensity(hypnodensity, showPlot = False, savePlot = False, fileName = 'tmp.png'):
+
+    fig, ax = plt.subplots(figsize=[11,5])
+    av = np.cumsum(hypnodensity,axis=1)
+    C = [[0.90, 0.19, 0.87],
+         [0.2 , 0.89, 0.93],
+         [0.22, 0.44, 0.73],
+         [0.34, 0.70, 0.39]]
+
+    for i in range(4):
+        xy = np.zeros([av.shape[0]*2,2])
+        xy[:av.shape[0],0] = np.arange(av.shape[0])
+        xy[av.shape[0]:,0] = np.flip(np.arange(av.shape[0]),axis=0)
+
+        xy[:av.shape[0],1] = av[:,i]
+        xy[av.shape[0]:,1] = np.flip(av[:,i+1],axis=0)
+
+        poly = Polygon(xy, facecolor=C[i], edgecolor=None)
+        ax.add_patch(poly)
+
+    plt.xlim([0, av.shape[0]])
+    # fig.savefig('test.png')
+    if savePlot:
+        fig.savefig(fileName)
+        #plt.savefig(fileName)
+
+
+    if showPlot:
+        print("Showing hypnodensity")
+        plt.show()
+
+
+class NarcoApp(object):
+
+    def __init__(self,appConfig):
+
+        # appConfig is an instance of AppConfig class, defined in inf_config.py
+        self.config = appConfig
+        self.edf_path = appConfig.edf_path  #full filename of an .EDF to use for header information.  A template .edf
+
+        self.Hypnodensity = Hypnodensity(appConfig)
+
+        self.models_used = appConfig.models_used
+
+        self.edfeatureInd = []
+        self.narco_features = []
+        self.narcolepsy_probability = []
+        # self.extract_features = ExtractFeatures(appConfig)  <-- now in Hypnodensity
+
+    def get_hypnodensity(self):
+        return self.Hypnodensity.get_hypnodensity()
+
+    def get_narco_gpmodels(self):
+        #gpmodels = [[]]*len(self.models_used)
+        #return gpmodels
+        return self.models_used
+
+    def get_hypnodensity_features(self, modelName, idx):
+        return self.Hypnodensity.get_features(modelName, idx)
+        # pdb.set_trace()
+        # selected_features  = self.config.narco_prediction_selected_features
+        # X = self.extract_features.extract(self.hypnodensity[idx])
+        # X = self.extract_features.scale_features(X,modelName)
+        # return X[selected_features].T
+
+    def get_narco_prediction(self):     # ,current_subset, num_subjects, num_models, num_folds):
+        # Initialize dummy variables
+        num_subjects = 1
+        gpmodels   = self.get_narco_gpmodels()
+        num_models = len(gpmodels)
+        num_folds  = self.config.narco_prediction_num_folds
+
+        mean_pred  = np.zeros([num_subjects, num_models, num_folds])
+        var_pred   = np.zeros([num_subjects, num_models, num_folds])
+
+        scales = self.config.narco_prediction_scales
+        gpmodels_base_path = self.config.narco_classifier_path
+
+        for idx, gpmodel in enumerate(gpmodels):
+            print('{} | Predicting using: {}'.format(datetime.now(), gpmodel))
+
+            if False:
+                # Load the data
+                mat_file_path = os.path.join(current_subset, gpmodel + '_testFeatures.mat')
+                X = sio.loadmat(mat_file_path)['features'].T
+                selected_features  = self.config.narco_prediction_selected_features
+                X = X[:, selected_features]
+            else:
+                X = self.get_hypnodensity_features(gpmodel, idx)
+
+            for k in range(num_folds):
+        #         print('{} | Loading and predicting using {}'.format(datetime.now(), os.path.join(gpmodels_base_path, gpmodel, gpmodel + '_fold{:02}.gpm'.format(k+1))))
+                with tf.Graph().as_default() as graph:
+                    with tf.Session():
+                        m = gpf.saver.Saver().load(os.path.join(gpmodels_base_path, gpmodel, gpmodel + '_fold{:02}.gpm'.format(k+1)))
+                        mean_pred[:, idx, k, np.newaxis], var_pred[:, idx, k, np.newaxis] = m.predict_y(X)
+
+        self.narcolepsy_probability = np.sum(np.multiply(np.mean(mean_pred, axis=2), scales), axis=1)/np.sum(scales)
+        return self.narcolepsy_probability
+
+    def plotHypnodensity(self):
+        self.Hypnodensity.renderHynodenisty(option = "plot")
+
+    def eval_hypnodensity(self):
+        self.Hypnodensity.evaluate()
+
+    def eval_narcolepsy(self):
+        self.get_narco_prediction()
+
+    def eval_all(self):
+        self.eval_hypnodensity()
+        self.eval_narcolepsy()
+
+if __name__ == '__main__':
+    outputFormat='json'
+
+    if sys.argv[2:]:  # if there are at least three arguments (two beyond [0])
+
+
+        if sys.argv[1]=='demo':
+
+            # print results
+            results  = {}
+
+            randomTest = sys.argv[2]=="random"
+            if randomTest:
+                random.seed()
+                ml_pred = random.triangular(0.0, 0.001, 0.95)
+            else:
+                ml_pred = 0.001
+
+            ml_threshold = 0.5
+            ml_result = ml_pred>=ml_threshold
+
+            edfObj = edfClass(filename="demo.edf");
+            results["machine_learning"] = (ml_result, ml_pred, ml_threshold)
+            results["reml"]=edfObj.testREMLatency(randomTest)
+            results["soremp"]=edfObj.testSOREMPs(randomTest)
+            results["nrem"]=edfObj.testNREMFragmentation(randomTest)
+            results["pwakes"]=edfObj.testPWakes(randomTest)
+
+            results["hypnogram"] = [1, 2, 3 , 4, 5 , 0 , 1 , 2 , 3]
+            results["hypnodensity_image"]='tmp.png'
+            print (json.dumps(results))
+
+        else:
+
+            edfFile = sys.argv[1]
+            #print(sys.argv[2]);
+            jsonObj = json.loads(sys.argv[2])
+            try:
+                main(edfFile,jsonObj)
+            except OSError as oserr:
+                print("OSError:",oserr)
+
+
+    else:
+        print(sys.argv[0],'requires two arguments when run as a script' )
---
+++ inf_network.py
@@ -0,0 +1,210 @@
+import inf_convolution as sc_conv
+
+import tensorflow as tf
+import pdb
+
+
+class SCModel(object):
+
+    def __init__(self, ac_config):
+        self.is_training = ac_config.is_training
+        # Placeholders
+
+        self._features = tf.placeholder(tf.float32, [None, None, ac_config.num_features], name='ModelInput')
+        self._targets = tf.placeholder(tf.float32, [None, ac_config.num_classes], name='ModelOutput')
+        self._mask = tf.placeholder(tf.float32, [None], name='ModelWeights')
+
+        # self._batch_size = tf.placeholder_with_default(ac_config.batch_size, [1,], name='BatchSize')
+        self._batch_size = tf.placeholder(tf.int32, name='BatchSize')
+        self._learning_rate = tf.placeholder(tf.float32, name='LearningRate')
+
+        batch_size_int = tf.reshape(self._batch_size, [])
+
+        if ac_config.lstm:
+            self._initial_state = tf.placeholder_with_default(
+                tf.zeros([batch_size_int, ac_config.num_hidden * 2], dtype=tf.float32), [None, ac_config.num_hidden * 2],
+                name='InitialState')
+
+        # Layer in
+        with tf.variable_scope('input_hidden') as scope:
+            inputs = self._features
+            inputs = tf.reshape(inputs, shape=[batch_size_int, -1, ac_config.segsize, ac_config.num_features])
+
+        hidden_eeg = sc_conv.main(inputs[:, :, :, :400], ac_config, 'eeg', batch_size_int)
+        hidden_eog = sc_conv.main(inputs[:, :, :, 400:1600], ac_config, 'eog', batch_size_int)
+        hidden_emg = sc_conv.main(inputs[:, :, :, 1600:], ac_config, 'emg', batch_size_int)
+
+        # For debugging
+        # pdb.set_trace()
+        if tf.__version__ < '1.0':
+            hidden_combined = tf.concat(2, [hidden_eeg, hidden_eog, hidden_emg])
+        else:
+            hidden_combined = tf.concat([hidden_eeg, hidden_eog, hidden_emg], 2)
+        nHid = hidden_combined.get_shape()
+
+        # Regularization
+
+        if ac_config.is_training and (
+                ac_config.keep_prob < 1.0):  # should this be ac_config.is_training + ac_config.keep_prob < 1.0
+            iKeepProb = ac_config.keep_prob
+            oKeepProb = ac_config.keep_prob
+        else:
+            iKeepProb = 1
+            oKeepProb = 1
+
+        # Layer hidden
+        with tf.variable_scope('hidden_hidden') as scope:
+            if ac_config.lstm:
+                cell = tf.nn.rnn_cell.BasicLSTMCell(ac_config.num_hidden, forget_bias=1.0, state_is_tuple=True)
+                cell = tf.nn.rnn_cell.DropoutWrapper(cell, input_keep_prob=iKeepProb, output_keep_prob=oKeepProb)
+                initial_state = tf.nn.rnn_cell.LSTMStateTuple(self._initial_state[:, :ac_config.num_hidden],
+                                                              self._initial_state[:, ac_config.num_hidden:])
+                outputs, final_state = tf.nn.dynamic_rnn(cell, hidden_combined, dtype=tf.float32,
+                                                         initial_state=initial_state)
+
+            else:
+                hidden_combined = tf.reshape(hidden_combined, [-1, int(nHid[2])])
+                weights = sc_conv._variable_with_weight_decay('weights', shape=[nHid[2], ac_config.num_hidden],
+                                                              stddev=0.04, wd=0.00001)
+                biases = sc_conv._variable_on_cpu('biases', ac_config.num_hidden, tf.constant_initializer(0.01))
+                outputs = tf.nn.relu(tf.add(tf.matmul(hidden_combined, weights), biases), name=scope.name)
+                # sc_conv._activation_summary(outputs)
+
+        # Layer out
+        with tf.variable_scope('hidden_output') as scope:
+            outputs = tf.reshape(outputs, [-1, ac_config.num_hidden])
+            weights = sc_conv._variable_with_weight_decay('weights', shape=[ac_config.num_hidden, ac_config.num_classes],
+                                                          stddev=0.04, wd=0.00001)
+            biases = sc_conv._variable_on_cpu('biases', ac_config.num_classes, tf.constant_initializer(0.001))
+            logits = tf.add(tf.matmul(outputs, weights), biases, name=scope.name)
+            # sc_conv._activation_summary(logits)
+
+        # Evaluate
+
+        cross_ent = self.intelligent_cost(logits)
+        loss = self.gather_loss()
+        self._loss = loss
+        self._logits = logits
+        self._cross_ent = cross_ent
+        self._softmax = tf.nn.softmax(logits)
+        self._predict = tf.argmax(self._softmax, 1)
+        self._correct = tf.equal(tf.argmax(logits, 1), tf.argmax(self._targets, 1))
+        self._accuracy = tf.reduce_mean(tf.cast(self._correct, tf.float32))
+        self._confidence = tf.reduce_sum(tf.multiply(self._softmax, self._targets), 1);
+        self._baseline = (tf.reduce_mean(self._targets, 0))
+
+        if ac_config.lstm:
+            if tf.__version__ < '1.0':
+                self._final_state = tf.concat(1, [final_state.c, final_state.h])
+            else:
+                self._final_state = tf.concat([final_state.c, final_state.h], 1)
+
+        if not ac_config.is_training:
+            return
+
+        # Optimize
+        optimizer = tf.train.MomentumOptimizer(learning_rate=self._learning_rate, momentum=0.9)
+        variables_averages = tf.train.ExponentialMovingAverage(0.999)
+
+        optimize = optimizer.minimize(self._loss)
+        variables_averages_op = variables_averages.apply(tf.trainable_variables())
+
+        with tf.control_dependencies([optimize, variables_averages_op]):
+            self._train_op = tf.no_op(name='train')
+
+    def intelligent_cost(self, logits):
+        logits = tf.clip_by_value(logits, -1e10, 1e+10)
+        cross_ent = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=self._targets)
+        # cross_ent = tf.mul(cross_ent, self._mask)
+        cross_ent = tf.reduce_mean(cross_ent)  # / tf.reduce_sum(self._mask)
+        tf.add_to_collection('losses', cross_ent)
+
+        return cross_ent
+
+    def gather_loss(self):
+
+        loss_averages = tf.train.ExponentialMovingAverage(0.9, name='avg_loss')
+        losses = tf.get_collection('losses')
+        total_loss = tf.add_n(losses, name='total_loss')
+        loss_averages_op = loss_averages.apply(losses + [total_loss])
+
+        # for l in losses + [total_loss]:
+        # Name each loss as '(raw)' and name the moving average version of the loss
+        # as the original loss name.
+
+        # tf.scalar_summary(total_loss.op.name +' (raw)', total_loss)
+        # tf.scalar_summary(total_loss.op.name, loss_averages.average(total_loss))
+
+        return total_loss
+
+    @property
+    def features(self):
+        return self._features
+
+    @property
+    def final_state(self):
+        return self._final_state
+
+    @property
+    def initial_state(self):
+        return self._initial_state
+
+    @property
+    def targets(self):
+        return self._targets
+
+    @property
+    def mask(self):
+        return self._mask
+
+    @property
+    def batch_size(self):
+        return self._batch_size
+
+    @property
+    def learning_rate(self):
+        return self._learning_rate
+
+    @property
+    def cost(self):
+        return self._cost
+
+    @property
+    def loss(self):
+        return self._loss
+
+    @property
+    def cross_ent(self):
+        return self._cross_ent
+
+    @property
+    def accuracy(self):
+        return self._accuracy
+
+    @property
+    def baseline(self):
+        return self._baseline
+
+    @property
+    def train_op(self):
+        return self._train_op
+
+    @property
+    def predict(self):
+        return self._predict
+
+    @property
+    def logits(self):
+        return self._logits
+
+    @property
+    def confidence(self):
+        return self._confidence
+
+    @property
+    def ar_prob(self):
+        return self._ar_prob
+
+    @property
+    def softmax(self):
+        return self._softmax
