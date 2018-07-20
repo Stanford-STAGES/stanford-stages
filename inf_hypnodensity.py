Bug fixes plus scaling .p files
Fixing refactoring errors in inf_hypnodensity.py
Added scaling.p files for the 16 models.


Former-commit-id: 6804e694b0ee39b79a68c2dee3dad3f1b7d590e5

inf_hypnodensity.py
@@ -343,13 +343,6 @@ class Hypnodensity(object):
         self.channels_used[self.channels[notUsedC]] = []
         self.channels_used[self.channels[notUsedO]] = []
 
-    def score_data(self):
-        self.hypnodensity = list()
-        for l in self.config.models_used:
-            hyp = inf_hypnodensity.run_data(self.encodedD,l,self.config.hypnodensity_model_root_path)
-            hyp = self.softmax(hyp)
-            self.hypnodensity.append(hyp)
-
     def softmax(self,x):
 
         #e_x = np.exp(x - np.max(x))
@@ -364,8 +357,17 @@ class Hypnodensity(object):
         ac_config = ACConfig(model_name=model, is_training=False, root_model_dir = root_model_path)
         #root_train_data_dir,
         #root_test_data_dir))
-        hyp = run(dat,ac_config)
+        hyp = Hypnodensity.run(dat,ac_config)
         return hyp
+
+    def score_data(self):
+        self.hypnodensity = list()
+        for l in self.config.models_used:
+            hyp = Hypnodensity.run_data(self.encodedD,l,self.config.hypnodensity_model_root_path)
+            hyp = self.softmax(hyp)
+            self.hypnodensity.append(hyp)
+
+
 
     def segment(dat,ac_config):
 
@@ -401,7 +403,7 @@ class Hypnodensity(object):
 
                 state = np.zeros([1,ac_config.num_hidden*2])
 
-                dat, Nextra, prediction, num_batches = segment(dat, ac_config)
+                dat, Nextra, prediction, num_batches = Hypnodensity.segment(dat, ac_config)
                 for i in range(num_batches):
 
                     x = dat[:,i*ac_config.eval_nseg_atonce*ac_config.segsize:(i+1)*ac_config.eval_nseg_atonce*ac_config.segsize,:]

inf_narco_app.py
@@ -17,9 +17,9 @@ warnings.simplefilter('ignore',FutureWarning)  #warnings.filterwarnings("ignore"
 # for getting predictions
 import numpy as np
 import scipy.io as sio
-import gpflow as gpf
-import tensorflow as tf
 
+import tensorflow as tf
+import gpflow as gpf
 import random
 
 # For hypnodensity plotting ...
@@ -141,17 +141,12 @@ class NarcoApp(object):
         return self.Hypnodensity.get_hypnodensity()
 
     def get_narco_gpmodels(self):
-        #gpmodels = [[]]*len(self.models_used)
-        #return gpmodels
+
         return self.models_used
 
     def get_hypnodensity_features(self, modelName, idx):
         return self.Hypnodensity.get_features(modelName, idx)
-        # pdb.set_trace()
-        # selected_features  = self.config.narco_prediction_selected_features
-        # X = self.extract_features.extract(self.hypnodensity[idx])
-        # X = self.extract_features.scale_features(X,modelName)
-        # return X[selected_features].T
+
 
     def get_narco_prediction(self):     # ,current_subset, num_subjects, num_models, num_folds):
         # Initialize dummy variables