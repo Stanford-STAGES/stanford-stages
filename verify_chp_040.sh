58e608be663073a4ec100a14330a6804235cad16
informaton <hyattmoore@gmail.com>
2018-11-12 11:37:56 -0800

    link changes
    Former-commit-id: fe208e3d99f7366cbf8ebb88e9ae2fc3ef8ea320

--- verify_chp_040.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-FILENAME="/Users/unknown/data/sleep/CHP040.edf"
-rm /Users/unknown/data/sleep/CHP040.*pkl
-
-python3 -W ignore inf_narco_app.py "$FILENAME" \
-  '{"channel_indices":{"centrals":[3,4],"occipitals":[5,6],"eog_l":7,"eog_r":8,"chin_emg":9}, "hypnodensity":{"showplot":true}}'
--- verify_ssc_7636.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-FILENAME="/Users/unknown/data/narco_test/SSC_7636_1.EDF"
-
-python3 -W ignore inf_narco_app.py "$FILENAME" \
-  '{"channel_indices":{"central":23,"occipital":34,"eog_l":33,"eog_r":37,"chin_emg":0}, "hypnodensity":{"showplot":true}}'
-
-
