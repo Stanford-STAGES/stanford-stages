97f917bcd8bfbd6fb8a51cbe60118b1daa36a2f8
informaton <hyattmoore@gmail.com>
2019-01-15 09:45:51 -0800

    updates to apply softmax directly
    Former-commit-id: c7b41c205e30abedd80a3e4ffa5e5c3886432e01

--- verify_chp_040.sh
+++ verify_chp_040.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 FILENAME="/Users/unknown/data/sleep/narcoTest/CHP040.edf"
 
-# rm /Users/unknown/data/sleep/narcoTest/CHP040.*pkl
+rm /Users/unknown/data/sleep/narcoTest/CHP040.*pkl
 
 python3 -W ignore inf_narco_app.py "$FILENAME" \
   '{"channel_indices":{"centrals":[3,4],"occipitals":[5,6],"eog_l":7,"eog_r":8,"chin_emg":9},
